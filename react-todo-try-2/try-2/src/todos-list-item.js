fa7982ddec0f0fc846d7548148486bd3b1e77a12
sandramk <sandramkillebrew@gmail.com>
2017-06-20 16:17:24 -0500

    render actions added

--- react-todo-try-2/try-2/src/todos-list-item.js
+++ react-todo-try-2/try-2/src/todos-list-item.js
@@ -1,15 +1,40 @@
 import React from 'react';
 
 export default class TodosListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isEditing: false
+    };
+}
+
+  renderActionsSection() {
+    if (this.state.isEditing) {
+      return (
+        <td>
+          <button>Save</button>
+          <button>Cancel</button>
+        </td>
+      );
+    }
+      return (
+        <td>
+          <button onClick={this.onEditClick.bind(this)}>Edit</button>
+          <button>Delete</button>
+        </td>
+      );
+  }
   render () {
     return (
         <tr>
           <td>{this.props.task}</td>
-          <td>
-            <button>Edit</button>
-            <button>Delete</button>
-          </td>
+          <td>{this.renderActionsSection()}</td>
         </tr>
     )
   }
+
+    onEditClick() {
+      this.setState({ isEditing: true});
+    }
 };
