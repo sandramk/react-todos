a0ee0570a78628b7aa8a734dde650dad56c0a7e2
sandramk <sandramkillebrew@gmail.com>
2017-06-20 16:43:08 -0500

    empty value

--- react-todo-try-2/try-2/src/create-todo.js
+++ react-todo-try-2/try-2/src/create-todo.js
@@ -16,5 +16,6 @@ export default class CreateTodo extends React.Component {
     event.preventDefault();
 
     this.props.createTask(this.refs.createInput.value);
+    this.refs.createInput.value= '';
   }
 }
