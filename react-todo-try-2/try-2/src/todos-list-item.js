eda12fe7b6a0ff52d790ca334b9c911ad39e6ec7
sandramk <sandramkillebrew@gmail.com>
2017-06-20 19:41:34 -0500

    save task

--- react-todo-try-2/try-2/src/todos-list-item.js
+++ react-todo-try-2/try-2/src/todos-list-item.js
@@ -17,6 +17,16 @@ export default class TodosListItem extends React.Component {
       cursor: 'pointer'
     }
 
+    if (this.state.isEditinng) {
+      return (
+        <td>
+          <form onSubmit={this.onSaveClick.bind(this)} >
+            <input type="text" defaultValue={task} ref="editInput"/>
+          </form>
+        </td>
+      )
+    }
+
     return (
       <td style={taskStyle}
           onClick={this.props.toggleTask.bind(this, task)}
@@ -30,7 +40,7 @@ export default class TodosListItem extends React.Component {
     if (this.state.isEditing) {
       return (
         <td>
-          <button>Save</button>
+          <button onClick={this.onSaveClick.bind(this)}>Save</button>
           <button onClick={this.onCancelClick.bind(this)}>Cancel</button>
         </td>
       );
@@ -57,5 +67,13 @@ export default class TodosListItem extends React.Component {
 
     onCancelClick() {
       this.setState({ isEditing:false });
+    }
+
+    onSaveClick(event) {
+      event.preventDefault();
+      const oldTask = this.props.task;
+      const newTask = this.refs.editInput.value;
+      this.props.saveTask(oldTask, newTask);
+      this.setState({ isEditing: false });
     }
 };
